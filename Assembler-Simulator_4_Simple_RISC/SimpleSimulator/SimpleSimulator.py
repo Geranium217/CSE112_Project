9d3fa84cb5b80193afe57af326c91fbf8d9db111
Aayush <aayush21225@iiitd.ac.in>
2022-08-07 00:45:43 +0530

    SUbmission

--- Assembler-Simulator_4_Simple_RISC/SimpleSimulator/SimpleSimulator.py
+++ Assembler-Simulator_4_Simple_RISC/SimpleSimulator/SimpleSimulator.py
@@ -2,17 +2,21 @@
 # from execution_engine import execution_engine as EE
 # from pc import PC
 
+import helper
+from machine import Machine as m
+Machine = m()
+Machine.Memory.Load() # Load from stdin
 
-MEM = memory()
-
+PC = 0
 
 halted = False # Machine is currently running
 
 while (not halted):
-    ins = MM.
-    PC.dump()
-    RF.dump()
-    PC.update(new_pc)
+    ins =  Machine.Memory.getData(PC)
+    halted , new_pc = Machine.execute(ins,PC)
+    
+    print(f"{helper.binary_number(PC,8)} ",end='')
+    print(Machine.RF.dump())
+    PC = new_pc
 
-MEM.dump()
-
+Machine.Memory.dump()
--- Assembler-Simulator_4_Simple_RISC/SimpleSimulator/memory.py
+++ Assembler-Simulator_4_Simple_RISC/SimpleSimulator/memory.py
@@ -1,10 +1,25 @@
 
 
-class memory:
+
+class Memory:
     def __init__(self):
-        self.memory = []
+        constant = '0'*16
+        self.memory = [ constant for x in range(256) ]
+    
+    def getData(self,number):
+        return self.memory[number]
+
+    def setData(self,number,data):
+        self.memory[number] = data
+        
+    def Load(self):
+        from sys import stdin
+        counter = 0
+        for line in stdin:
+            line = line.rstrip('\n')
+            self.setData(counter,line)
+            counter+=1
 
     def dump(self):
-        
-
-
+        for i in self.memory:
+            print(i)
