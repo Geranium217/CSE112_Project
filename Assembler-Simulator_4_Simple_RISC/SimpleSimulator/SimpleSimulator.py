d316b790e6e09ebff75759a9c5f5e10f66eda102
Aayush <aayush21225@iiitd.ac.in>
2022-08-06 23:59:53 +0530

    Final

---
+++ Assembler-Simulator_4_Simple_RISC/SimpleSimulator/SimpleSimulator.py
@@ -0,0 +1,18 @@
+# from memory import memory
+# from execution_engine import execution_engine as EE
+# from pc import PC
+
+
+MEM = memory()
+
+
+halted = False # Machine is currently running
+
+while (not halted):
+    ins = MM.
+    PC.dump()
+    RF.dump()
+    PC.update(new_pc)
+
+MEM.dump()
+
---
+++ Assembler-Simulator_4_Simple_RISC/SimpleSimulator/memory.py
@@ -0,0 +1,10 @@
+
+
+class memory:
+    def __init__(self):
+        self.memory = []
+
+    def dump(self):
+        
+
+
